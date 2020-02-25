feb3424b4d13c224d8261a9c018feb5d9ab50c8d
Jacob Quinn Shenker <jacob@shenker.net>
2020-02-25 13:43:03 -0500

    Switched from single to double quotes.

--- nbcleanse.py
+++ nbcleanse.py
@@ -66,14 +66,14 @@ def pop_recursive(d, key, default=None):
     >>> d
     {'a': {'b': 1}}
     """
-    nested = key.split('.')
+    nested = key.split(".")
     current = d
     for k in nested[:-1]:
-        if hasattr(current, 'get'):
+        if hasattr(current, "get"):
             current = current.get(k, {})
         else:
             return default
-    if not hasattr(current, 'pop'):
+    if not hasattr(current, "pop"):
         return default
     return current.pop(nested[-1], default)
 
@@ -98,56 +98,56 @@ def strip_jupyter(
     or counts.
     `extra_keys` could be `('metadata.foo', 'cell.metadata.bar', 'metadata.baz')`
     """
-    if keep_output is None and 'keep_output' in nb.metadata:
-        keep_output = bool(nb.metadata['keep_output'])
-    keys = {'metadata': [], 'cell': {'metadata': []}}
+    if keep_output is None and "keep_output" in nb.metadata:
+        keep_output = bool(nb.metadata["keep_output"])
+    keys = {"metadata": [], "cell": {"metadata": []}}
     for key in extra_keys:
-        if key.startswith('metadata.'):
-            keys['metadata'].append(key[len('metadata.') :])
-        elif key.startswith('cell.metadata.'):
-            keys['cell']['metadata'].append(key[len('cell.metadata.') :])
+        if key.startswith("metadata."):
+            keys["metadata"].append(key[len("metadata.") :])
+        elif key.startswith("cell.metadata."):
+            keys["cell"]["metadata"].append(key[len("cell.metadata.") :])
         else:
             print(f"ignoring extra key {key}", file=sys.stderr)
-    for field in keys['metadata']:
+    for field in keys["metadata"]:
         pop_recursive(nb.metadata, field)
     failed_cells = 0
     for cell in _cells(nb):
-        if cell['cell_type'] == 'code':
+        if cell["cell_type"] == "code":
             # Remove the prompt_number/execution_count, unless directed otherwise
             if not keep_count:
-                cell.pop('prompt_number', None)
-                cell['execution_count'] = None
-            source = cell['source']
-            source = re.sub('^%', '#%#', source, flags=re.M)
+                cell.pop("prompt_number", None)
+                cell["execution_count"] = None
+            source = cell["source"]
+            source = re.sub("^%", "#%#", source, flags=re.M)
             new_source = blacken(source)
             if new_source is not None:
                 new_source = new_source.rstrip()
-                new_source = re.sub('^#%#', '%', new_source, flags=re.M)
-                cell['source'] = new_source
+                new_source = re.sub("^#%#", "%", new_source, flags=re.M)
+                cell["source"] = new_source
             else:
                 failed_cells += 1
         keep_output_this_cell = keep_output
         # Keep the output for these cells, but strip count and metadata
-        if 'keep_output' in cell.metadata:
-            keep_output_this_cell = bool(cell.metadata['keep_output'])
+        if "keep_output" in cell.metadata:
+            keep_output_this_cell = bool(cell.metadata["keep_output"])
         # Remove the outputs, unless directed otherwise
-        if 'outputs' in cell:
+        if "outputs" in cell:
             # Default behavior strips outputs. With all outputs stripped,
             # there are no counts to keep and keep_count is ignored.
             if not keep_output_this_cell:
-                cell['outputs'] = []
+                cell["outputs"] = []
             # If keep_output_this_cell, but not keep_count, strip the counts
             # from the output.
             if keep_output_this_cell and not keep_count:
-                for output in cell['outputs']:
-                    if 'execution_count' in output:
-                        output['execution_count'] = None
+                for output in cell["outputs"]:
+                    if "execution_count" in output:
+                        output["execution_count"] = None
             # If keep_output_this_cell and keep_count, do nothing.
         # Always remove this metadata
-        if 'metadata' in cell:
-            for field in ['collapsed', 'scrolled']:
+        if "metadata" in cell:
+            for field in ["collapsed", "scrolled"]:
                 cell.metadata.pop(field, None)
-        for (extra, fields) in keys['cell'].items():
+        for (extra, fields) in keys["cell"].items():
             if extra in cell:
                 for field in fields:
                     pop_recursive(getattr(cell, extra), field)
@@ -172,19 +172,19 @@ def filter_jupyter(contents, filename):
     return new_contents.encode()
 
 
-filetype_filters = {'py': filter_py, 'ipynb': filter_jupyter}
+filetype_filters = {"py": filter_py, "ipynb": filter_jupyter}
 
 
 def filter_commit(commit, metadata):
     for change in commit.file_changes:
         filename = change.filename.decode()
-        extension = filename.split('.')[-1].lower()
+        extension = filename.split(".")[-1].lower()
         if change.blob_id in blobs_handled:
             change.blob_id = blobs_handled[change.blob_id]
         elif extension in filetype_filters.keys():
             # change.blob_id is None for deleted files (ex: change.type=b'D')
             if change.blob_id is None:
-                assert change.type == b'D'
+                assert change.type == b"D"
                 continue
             # Get the old blob contents
             cat_file_process.stdin.write(change.blob_id + b"\n")
@@ -215,7 +215,7 @@ def filter_repo():
     args = fr.FilteringOptions.default_options()
     args.force = True
     cat_file_process = subprocess.Popen(
-        ['git', 'cat-file', '--batch'], stdin=subprocess.PIPE, stdout=subprocess.PIPE
+        ["git", "cat-file", "--batch"], stdin=subprocess.PIPE, stdout=subprocess.PIPE
     )
     repo_filter = fr.RepoFilter(args, commit_callback=filter_commit)
     repo_filter.run()
@@ -224,43 +224,43 @@ def filter_repo():
 
 
 @cli.command()
-@click.option('--gitattrs', default=None, help='Location of .gitattributes file')
+@click.option("--gitattrs", default=None, help="Location of .gitattributes file")
 def install(gitattrs=None):
     """Install the git filter and set the git attributes."""
     from os import name, path
     from subprocess import check_call, check_output, CalledProcessError
 
     try:
-        git_dir = check_output(['git', 'rev-parse', '--git-dir']).strip()
-    except (WindowsError if name == 'nt' else OSError):
-        print('Installation failed: git is not on path!', file=sys.stderr)
+        git_dir = check_output(["git", "rev-parse", "--git-dir"]).strip()
+    except (WindowsError if name == "nt" else OSError):
+        print("Installation failed: git is not on path!", file=sys.stderr)
         sys.exit(1)
     except CalledProcessError:
-        print('Installation failed: not a git repository!', file=sys.stderr)
+        print("Installation failed: not a git repository!", file=sys.stderr)
         sys.exit(1)
     filepath = "'{}' '{}' filter".format(
-        sys.executable.replace('\\', '/'), path.abspath(__file__)
+        sys.executable.replace("\\", "/"), path.abspath(__file__)
     )
-    check_call(['git', 'config', 'filter.nbcleanse.clean', filepath])
-    check_call(['git', 'config', 'filter.nbcleanse.smudge', 'cat'])
-    check_call(['git', 'config', 'diff.ipynb.textconv', filepath + ' -t'])
+    check_call(["git", "config", "filter.nbcleanse.clean", filepath])
+    check_call(["git", "config", "filter.nbcleanse.smudge", "cat"])
+    check_call(["git", "config", "diff.ipynb.textconv", filepath + " -t"])
 
     if not gitattrs:
-        gitattrs = path.join(git_dir.decode(), 'info', 'attributes')
+        gitattrs = path.join(git_dir.decode(), "info", "attributes")
     gitattrs = path.expanduser(gitattrs)
 
     # Check if there is already a filter for ipynb files
     filter_exists = False
     diff_exists = False
     if path.exists(gitattrs):
-        with open(gitattrs, 'r') as f:
+        with open(gitattrs, "r") as f:
             attrs = f.read()
-        filter_exists = '*.ipynb filter' in attrs
-        diff_exists = '*.ipynb diff' in attrs
+        filter_exists = "*.ipynb filter" in attrs
+        diff_exists = "*.ipynb diff" in attrs
         if filter_exists and diff_exists:
             return
 
-    with open(gitattrs, 'a') as f:
+    with open(gitattrs, "a") as f:
         # If the file already exists, ensure it ends with a new line
         if f.tell():
             f.write("\n")
@@ -271,44 +271,44 @@ def install(gitattrs=None):
 
 
 @cli.command()
-@click.option('--gitattrs', default=None, help='Location of .gitattributes file')
+@click.option("--gitattrs", default=None, help="Location of .gitattributes file")
 def uninstall(gitattrs):
     """Uninstall the git filter and unset the git attributes."""
     from os import devnull, path
     from subprocess import call, check_output, CalledProcessError, STDOUT, DEVNULL
 
     try:
-        git_dir = check_output(['git', 'rev-parse', '--git-dir']).strip()
+        git_dir = check_output(["git", "rev-parse", "--git-dir"]).strip()
     except CalledProcessError:
-        print('Installation failed: not a git repository!', file=sys.stderr)
+        print("Installation failed: not a git repository!", file=sys.stderr)
         sys.exit(1)
     call(
-        ['git', 'config', '--unset', 'filter.nbcleanse.clean'],
+        ["git", "config", "--unset", "filter.nbcleanse.clean"],
         stdout=DEVNULL,
         stderr=STDOUT,
     )
     call(
-        ['git', 'config', '--unset', 'filter.nbcleanse.smudge'],
+        ["git", "config", "--unset", "filter.nbcleanse.smudge"],
         stdout=DEVNULL,
         stderr=STDOUT,
     )
     call(
-        ['git', 'config', '--remove-section', 'diff.ipynb'],
+        ["git", "config", "--remove-section", "diff.ipynb"],
         stdout=DEVNULL,
         stderr=STDOUT,
     )
     if not gitattrs:
-        gitattrs = path.join(git_dir.decode(), 'info', 'attributes')
+        gitattrs = path.join(git_dir.decode(), "info", "attributes")
     # Check if there is a filter for ipynb files
     if path.exists(gitattrs):
-        with open(gitattrs, 'r+') as f:
+        with open(gitattrs, "r+") as f:
             lines = [
                 l
                 for l in f
-                if not (l.startswith('*.ipynb filter') or l.startswith('*.ipynb diff'))
+                if not (l.startswith("*.ipynb filter") or l.startswith("*.ipynb diff"))
             ]
             f.seek(0)
-            f.write(''.join(lines))
+            f.write("".join(lines))
             f.truncate()
 
 
@@ -323,27 +323,27 @@ def status():
     try:
         git_dir = path.dirname(
             path.abspath(
-                check_output(['git', 'rev-parse', '--git-dir']).strip().decode()
+                check_output(["git", "rev-parse", "--git-dir"]).strip().decode()
             )
         )
         clean = (
-            check_output(['git', 'config', 'filter.nbcleanse.clean']).decode().strip()
+            check_output(["git", "config", "filter.nbcleanse.clean"]).decode().strip()
         )
         smudge = (
-            check_output(['git', 'config', 'filter.nbcleanse.smudge']).decode().strip()
+            check_output(["git", "config", "filter.nbcleanse.smudge"]).decode().strip()
         )
-        diff = check_output(['git', 'config', 'diff.ipynb.textconv']).decode().strip()
+        diff = check_output(["git", "config", "diff.ipynb.textconv"]).decode().strip()
         attributes = (
-            check_output(['git', 'check-attr', 'filter', '--', '*.ipynb'])
+            check_output(["git", "check-attr", "filter", "--", "*.ipynb"])
             .decode()
             .strip()
         )
         diff_attributes = (
-            check_output(['git', 'check-attr', 'diff', '--', '*.ipynb'])
+            check_output(["git", "check-attr", "diff", "--", "*.ipynb"])
             .decode()
             .strip()
         )
-        if attributes.endswith('unspecified'):
+        if attributes.endswith("unspecified"):
             print(f"nbcleanse is not installed in repository {git_dir}")
             sys.exit(1)
         print(
@@ -371,36 +371,36 @@ def status():
 
 @cli.command()
 @click.option(
-    '-t',
-    '--textconv',
+    "-t",
+    "--textconv",
     is_flag=True,
     default=False,
-    help='Print filtered files to stdout',
+    help="Print filtered files to stdout",
 )
 @click.option(
-    '--keep-count',
+    "--keep-count",
     is_flag=True,
     default=False,
-    help='Do not strip execution count/prompt number',
+    help="Do not strip execution count/prompt number",
 )
-@click.option('--keep-output', is_flag=True, default=False, help='Do not strip output')
-@click.option('-s', '--strip-key', multiple=True, help='Strip key from notebook JSON')
-@click.argument('files', type=click.File('r', lazy=True), nargs=-1)
+@click.option("--keep-output", is_flag=True, default=False, help="Do not strip output")
+@click.option("-s", "--strip-key", multiple=True, help="Strip key from notebook JSON")
+@click.argument("files", type=click.File("r", lazy=True), nargs=-1)
 def filter(files, textconv, keep_count, keep_output, strip_key):
     # https://stackoverflow.com/a/16549381
     if sys.stdin:
-        sys.stdin.reconfigure(encoding='utf-8')
+        sys.stdin.reconfigure(encoding="utf-8")
         if not files:
             files = [sys.stdin]
             textconv = True
-    sys.stdout.reconfigure(encoding='utf-8')
-    sys.stderr.reconfigure(encoding='utf-8')
+    sys.stdout.reconfigure(encoding="utf-8")
+    sys.stderr.reconfigure(encoding="utf-8")
     if textconv:
         out_file = sys.stdout
 
     for file in files:
         if not textconv:
-            file.mode = 'r+'
+            file.mode = "r+"
             out_file = file
         try:
             nb = nbformat.read(file, as_version=nbformat.NO_CONVERT)
@@ -427,5 +427,5 @@ def main():
     cli()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
